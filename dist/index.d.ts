ef91cb1e7588c9bcfb38cdf7dafeadacc4d906b7
Prabeen04 <prabeen.strange@gmail.com>
2019-12-01 20:19:05 +0530

    webpack run from npm

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function explore(): number;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.ts",
+  // webpack will take the files from ./src/index
+  entry: "./index",
+
+  // and output it into /dist as bundle.js
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+
+  // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
   module: {
     rules: [
+      // we use babel-loader to load our jsx and tsx files
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
   }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: "./src/index.html"
+  //   })
+  // ]
 };
