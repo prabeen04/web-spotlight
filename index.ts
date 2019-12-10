d3031cc214ea9616ae37db8864019bf90c7cd997
Prabeen04 <prabeen.strange@gmail.com>
2019-12-10 22:16:31 +0530

    more configuration

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ export function spotlight(): string {
   return 'web-spotlight'
 }
 export function getHTML(node: HTMLElement, element: HTMLElement): void {
+  console.log(node)
+  console.log(element)
+  debugger
   if (!window.document) {
     throw new Error('Browser not detected!. run in browser')
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+  devtool: "inline-sourcemap",
   entry: "./index.ts",
   output: {
     path: path.join(__dirname, "/dist"),
