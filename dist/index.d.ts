b7042578646388f923464521279f81b2c96d1b2d
Prabeen04 <prabeen.strange@gmail.com>
2019-12-11 22:03:40 +0530

    xtra methods deleted

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,1 @@
-export declare function explore(): number;
-export declare function spotlight(): string;
 export declare function getHTML(node: HTMLElement, element: HTMLElement): void;
--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-export function explore(): number {
-  return 6
-}
-export function spotlight(): string {
-  return 'web-spotlight'
-}
 export function getHTML(node: HTMLElement, element: HTMLElement): void {
   console.log(node)
   console.log(element)
