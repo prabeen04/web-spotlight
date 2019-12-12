cae9ea81397d33102ca3f3527e0d5331433f8733
Prabeen04 <prabeen.strange@gmail.com>
2019-12-12 20:16:23 +0530

    Component created

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+export { default as Component } from './src/Component'
 export function getHTML(node: HTMLDivElement, element: any): void {
   if (!window.document) {
     throw new Error('Browser not detected!. run in browser')
