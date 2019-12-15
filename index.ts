1b5b3ecdda0d1fa0bf15e4628cadd09c13639c02
Prabeen04 <prabeen.strange@gmail.com>
2019-12-15 19:58:24 +0530

    event litsener moved to spotlight

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
-import * as React from "react";
 export { default as Spotlight } from './src/Spotlight'
 export function getHTML(node: HTMLDivElement, element: any): void {
-  function handleKeyPress(e: KeyboardEvent) {
-    console.log(e)
-    debugger
-  }
-  React.useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress)
-    return () => window.removeEventListener('keydown', handleKeyPress)
-  }, [])
+
   if (!window.document) {
     throw new Error('Browser not detected!. run in browser')
   }
--- src/Spotlight.tsx
+++ src/Spotlight.tsx
@@ -5,5 +5,13 @@ interface IProps {
 }
 
 export default function Spotlight(props: IProps): React.ReactElement {
+  function handleKeyPress(e: KeyboardEvent) {
+    console.log(e);
+    debugger;
+  }
+  React.useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
   return <div>{props.component}</div>;
 }
